07be629ef708cc9f092a7a46678d4cf76bd29700
Igor Pugachev <ipugachev84@gmail.com>
2019-01-29 18:26:58 +0000

    Locally add forked ReactSelectSimpleValue component
    The code from npm doesn't allow to use arrays as the values.
    So I have to fork the app locally and make changes

---
+++ src/lib/react-select-simple-value.js
@@ -0,0 +1,36 @@
+// Utils
+
+function flatten(arr) {
+  return arr.reduce(
+    (acc, val) => (Array.isArray(val.options) ? acc.concat(flatten(val.options)) : acc.concat(val)),
+    [],
+  );
+}
+
+function getValue(opts, val, getOptVal, isMulti) {
+  if (val === undefined) return undefined;
+
+  const options = flatten(opts);
+  const value = isMulti ? options.filter(o => val.includes(getOptVal(o))) : options.find(o => getOptVal(o) == val);
+  return value;
+}
+
+// Component
+
+const defaultGetOptionValue = opt => opt.value;
+
+function ReactSelectSimpleValue({
+  children,
+  defaultValue: simpleDefault,
+  getOptionValue = defaultGetOptionValue,
+  isMulti = false,
+  options,
+  value: simpleValue,
+}) {
+  const value = getValue(options, simpleValue, getOptionValue, isMulti);
+  const defaultValue = getValue(options, simpleDefault, getOptionValue, isMulti);
+
+  return children({ defaultValue, getOptionValue, isMulti, options, value });
+}
+
+export default ReactSelectSimpleValue;
