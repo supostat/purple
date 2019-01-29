6d785e5acc52c107362279500fa4121a63b7559c
Igor Pugachev <ipugachev84@gmail.com>
2019-01-29 18:22:51 +0000

    Add common constants

---
+++ src/constants/user.js
@@ -0,0 +1,11 @@
+export const USER_ENABLED_STATUS = 'enabled';
+export const USER_DISABLED_STATUS = 'disabled';
+
+export const USER_INVITE_PENDING_STATUS = 'pending';
+export const USER_INVITE_REVOKED_STATUS = 'revoked';
+export const USER_INVITE_ACCEPTED_STATUS = 'accepted';
+
+export const USER_STATUSES = {
+  [USER_ENABLED_STATUS]: 'Enabled',
+  [USER_DISABLED_STATUS]: 'Disabled',
+};
