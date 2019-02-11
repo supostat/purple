6ae0cb89374ab239b4c35741662a2b5766e401f3
Igor Pugachev <ipugachev84@gmail.com>
2019-02-11 14:08:57 +0000

    Update ApiErrorsMiddleware

--- src/redux/api-errors-middleware.js
+++ src/redux/api-errors-middleware.js
@@ -1,6 +1,8 @@
 import { HttpError } from 'found';
-import AuthService from '~/utils/auth-service';
+import oFetch from 'o-fetch';
+import { AuthService, getSafe } from '~/utils';
 import { showNotification } from '~/components/notification';
+import { getAuthUser } from './selectors';
 
 export default store => next => action => {
   if (action.meta && action.meta.message) {
@@ -10,16 +12,25 @@ export default store => next => action => {
   if (action.payload && action.payload.name === 'ApiError') {
     const { status } = action.payload;
     switch (status) {
-      case 404:
+      case 404: {
         throw new HttpError(404);
-      case 403:
-        showNotification({ text: 'This action is not permitted', type: 'error', position: 'topCenter' });
+      }
+      case 403: {
+        const message = getSafe(() => action.payload.response.message) || `This action is not permitted`;
+        showNotification({
+          text: `${message}`,
+          type: 'error',
+          position: 'topCenter',
+        });
         throw new HttpError(403);
-      case 401:
+      }
+      case 401: {
         AuthService.clearJwtToken();
         throw new HttpError(401);
-      default:
+      }
+      default: {
         return next(action);
+      }
     }
   }
   return next(action);
