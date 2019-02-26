0c8f83f80fdbb7d527623b4ca659c5eea12df223
Igor Pugachev <ipugachev84@gmail.com>
2019-02-26 09:53:22 +0000

    Add SomethingWentWrong page

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -2,3 +2,4 @@ export { default as SuccessPage } from './success-page';
 export { default as Page401 } from './page-401';
 export { default as Page403 } from './page-403';
 export { default as Page404 } from './page-404';
+export { default as SomethingWentWrong } from './something-went-wrong';
