d42105edd0f10882e0560549e36804c15973504b
Igor Pugachev <ipugachev84@gmail.com>
2019-02-28 08:59:20 +0000

    Add ADMIN_EMAIL env

--- webpack/production.config.js
+++ webpack/production.config.js
@@ -4,7 +4,11 @@ const getConfig = require('../webpack.config');
 
 module.exports = env => {
   const BASE_API_URL = oFetch(process.env, 'BASE_API_URL');
-  const config = getConfig(env, { [`process.env.BASE_API_URL`]: JSON.stringify(BASE_API_URL) });
+  const ADMIN_EMAIL = oFetch(process.env, 'ADMIN_EMAIL');
+  const config = getConfig(env, {
+    [`process.env.BASE_API_URL`]: JSON.stringify(BASE_API_URL),
+    [`process.env.ADMIN_EMAIL`]: JSON.stringify(ADMIN_EMAIL),
+  });
 
   config.mode = 'none';
   config.devtool = 'source-map';
